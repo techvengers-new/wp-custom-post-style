7695434b7b75b724ecd55a3b94f6be29b8cf2eb6
shahzaibtech <92725187+shahzaibtech@users.noreply.github.com>
2021-12-24 16:09:24 +0500

    Update register-post-type.php

--- register-post-type.php
+++ register-post-type.php
@@ -1,5 +1,5 @@
 <?php
-function pluginprefix_setup_post_type() { bubyu
+function pluginprefix_setup_post_type() {
     register_post_type( 'book', ['public' => true ] ); 
 } 
 add_action( 'init', 'pluginprefix_setup_post_type' );
