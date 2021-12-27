7edde07ff0cecb26f0b7271d14fed2978d3f0ea9
shahzaibtech <92725187+shahzaibtech@users.noreply.github.com>
2021-12-27 17:34:34 +0500

    Update wp-custom-post-style.php

--- wp-custom-post-style.php
+++ wp-custom-post-style.php
@@ -66,3 +66,39 @@ function plugin_activate_tech() {
 // 	<input type="file" name="doc"/>
 // 	<input type="submit" name="submit"/>
 // </form> -->
+
+
+function wporg_options_page_html() {
+    // check user capabilities
+    if ( ! current_user_can( 'manage_options' ) ) {
+        return;
+    }
+ 
+    // add error/update messages
+ 
+    // check if the user have submitted the settings
+    // WordPress will add the "settings-updated" $_GET parameter to the url
+    if ( isset( $_GET['settings-updated'] ) ) {
+        // add settings saved message with the class of "updated"
+        add_settings_error( 'wporg_messages', 'wporg_message', __( 'Settings Saved', 'wporg' ), 'updated' );
+    }
+ 
+    // show error/update messages
+    settings_errors( 'wporg_messages' );
+    ?>
+    <div class="wrap">
+        <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+        <form action="options.php" method="post">
+            <?php
+            // output security fields for the registered setting "wporg"
+            settings_fields( 'wporg' );
+            // output setting sections and their fields
+            // (sections are registered for "wporg", each field is registered to a specific section)
+            do_settings_sections( 'wporg' );
+            // output save settings button
+            submit_button( 'Save Settings' );
+            ?>
+        </form>
+    </div>
+    <?php
+}
