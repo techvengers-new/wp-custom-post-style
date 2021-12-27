9175888fbb1d8855d9279e587c93fa8e8834aa3b
daniyal7321 <76730101+daniyal7321@users.noreply.github.com>
2021-12-27 17:11:25 +0500

    s

--- wp-custom-post-style.php
+++ wp-custom-post-style.php
@@ -7,33 +7,62 @@
  * Author URI:        https://techvengers.com
  * Text Domain:       wp-custom-post-style
  */
-$con=mysqli_connect('localhost','root','','youtube');
-if(isset($_POST['submit'])){
-	$file=$_FILES['doc']['tmp_name'];
+register_activation_hook( __FILE__, 'plugin_activate_tech' );
+
+global $jal_db_version;
+$jal_db_version = '1.0';
+
+function plugin_activate_tech() {
+	global $wpdb;
+	global $jal_db_version;
+
+	$table_name = $wpdb->prefix . 'liveshoutbox';
 	
-	$ext=pathinfo($_FILES['doc']['name'],PATHINFO_EXTENSION);
-	if($ext=='xlsx'){
-		require('PHPExcel/PHPExcel.php');
-		require('PHPExcel/PHPExcel/IOFactory.php');
-		
-		
-		$obj=PHPExcel_IOFactory::load($file);
-		foreach($obj->getWorksheetIterator() as $sheet){
-			$getHighestRow=$sheet->getHighestRow();
-			for($i=0;$i<=$getHighestRow;$i++){
-				$name=$sheet->getCellByColumnAndRow(0,$i)->getValue();
-				$email=$sheet->getCellByColumnAndRow(1,$i)->getValue();
-				if($name!=''){
-					mysqli_query($con,"insert into user(name,email) values('$name','$email')");
-				}
-			}
-		}
-	}else{
-		echo "Invalid file format";
-	}
+	$charset_collate = $wpdb->get_charset_collate();
+
+	$sql = "CREATE TABLE $table_name (
+		id mediumint(9) NOT NULL AUTO_INCREMENT,
+		time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+		name tinytext NOT NULL,
+		text text NOT NULL,
+		url varchar(55) DEFAULT '' NOT NULL,
+		PRIMARY KEY  (id)
+	) $charset_collate;";
+
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+
+	add_option( 'jal_db_version', $jal_db_version );
 }
+
+
+
+// if(isset($_POST['submit'])){
+// 	$file=$_FILES['doc']['tmp_name'];
+	
+// 	$ext=pathinfo($_FILES['doc']['name'],PATHINFO_EXTENSION);
+// 	if($ext=='xlsx'){
+// 		require('PHPExcel/PHPExcel.php');
+// 		require('PHPExcel/PHPExcel/IOFactory.php');
+		
+		
+// 		$obj=PHPExcel_IOFactory::load($file);
+// 		foreach($obj->getWorksheetIterator() as $sheet){
+// 			$getHighestRow=$sheet->getHighestRow();
+// 			for($i=0;$i<=$getHighestRow;$i++){
+// 				$name=$sheet->getCellByColumnAndRow(0,$i)->getValue();
+// 				$email=$sheet->getCellByColumnAndRow(1,$i)->getValue();
+// 				if($name!=''){
+// 					mysqli_query($con,"insert into user(name,email) values('$name','$email')");
+// 				}
+// 			}
+// 		}
+// 	}else{
+// 		echo "Invalid file format";
+// 	}
+// }
 ?>
-<form method="post" enctype="multipart/form-data">
-	<input type="file" name="doc"/>
-	<input type="submit" name="submit"/>
-</form>
+<!-- // <form method="post" enctype="multipart/form-data">
+// 	<input type="file" name="doc"/>
+// 	<input type="submit" name="submit"/>
+// </form> -->
