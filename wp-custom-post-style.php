17525b37c0811bc334934dfc4600cda1a92c3547
shahzaibtech <92725187+shahzaibtech@users.noreply.github.com>
2021-12-28 10:52:34 +0500

    Update wp-custom-post-style.php

--- wp-custom-post-style.php
+++ wp-custom-post-style.php
@@ -34,34 +34,11 @@ function plugin_activate_tech() {
 
 register_activation_hook( __FILE__, 'plugin_activate_tech' );
 
+function wp_custom_settings(){
 
+}
 
-// if(isset($_POST['submit'])){
-// 	$file=$_FILES['doc']['tmp_name'];
-	
-// 	$ext=pathinfo($_FILES['doc']['name'],PATHINFO_EXTENSION);
-// 	if($ext=='xlsx'){
-// 		require('PHPExcel/PHPExcel.php');
-// 		require('PHPExcel/PHPExcel/IOFactory.php');
-		
-		
-// 		$obj=PHPExcel_IOFactory::load($file);
-// 		foreach($obj->getWorksheetIterator() as $sheet){
-// 			$getHighestRow=$sheet->getHighestRow();
-// 			for($i=0;$i<=$getHighestRow;$i++){
-// 				$name=$sheet->getCellByColumnAndRow(0,$i)->getValue();
-// 				$email=$sheet->getCellByColumnAndRow(1,$i)->getValue();
-// 				if($name!=''){
-// 					mysqli_query($con,"insert into user(name,email) values('$name','$email')");
-// 				}
-// 			}
-// 		}
-// 	}else{
-// 		echo "Invalid file format";
-// 	}
-// }
-?>
-<!-- // <form method="post" enctype="multipart/form-data">
-// 	<input type="file" name="doc"/>
-// 	<input type="submit" name="submit"/>
-// </form> -->
+function register_my_menu(){
+	add_menu_page('WP Custom','WP Custom Settings','manage_options','wp-custom-settings','wp_custom_settings','dashicons-admin-generic',50);
+}
+add_action('admin_menu','register_my_menu');
